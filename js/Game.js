db04266fe8964fe2962d1caccf0c717bff3afbac
Rohald van Merode <rohald_89@live.nl>
2020-11-11 00:55:02 +0100

    added styling to the game screen

--- js/Game.js
+++ js/Game.js
@@ -1,21 +1,21 @@
-class Game{
-    constructor(){
+class Game {
+    constructor() {
         this.missed = 0;
-        this.phrases = ["this will be phrase ONE", 
-                        "this will be phrase TWO", 
-                        "this will be phrase THREE", 
-                        "this will be phrase FOUR", 
-                        "this will be phrase FIVE"];
+        this.phrases = ["this will be phrase ONE",
+            "this will be phrase TWO",
+            "this will be phrase THREE",
+            "this will be phrase FOUR",
+            "this will be phrase FIVE"];
         this.activePhrase = null;
     }
 
-     /**
-      * If the overlay has the win or lose class reset the game
-      * hide the overlay, get a new random phrase and add it to the display
-      */
-    startGame(){
+    /**
+     * If the overlay has the win or lose class reset the game
+     * hide the overlay, get a new random phrase and add it to the display
+     */
+    startGame() {
         const overlay = document.querySelector('#overlay');
-        if(overlay.className === 'win' || overlay.className === 'lose'){
+        if (overlay.className === 'win' || overlay.className === 'lose') {
             this.resetGame();
         }
         overlay.style.display = 'none';
@@ -23,28 +23,28 @@ class Game{
         this.activePhrase.addPhraseToDisplay();
     }
 
-     /**
-      *  randomly select a phrase from the phrases array
-      */
-    getRandomPhrase(){
+    /**
+     *  randomly select a phrase from the phrases array
+     */
+    getRandomPhrase() {
         return this.phrases[Math.floor(Math.random() * this.phrases.length)];
     }
 
-     /**
-      * Disable clicked button, if the letter is not present in the phrase add the wrong class and remove a life
-      * otherwise add the chosen class, show the letters 
-      * check if the game is won, if so run the game over function
-      */
-    handleInteraction(clickedButton){
-        if(clickedButton.tagName === "BUTTON"){
+    /**
+     * Disable clicked button, if the letter is not present in the phrase add the wrong class and remove a life
+     * otherwise add the chosen class, show the letters 
+     * check if the game is won, if so run the game over function
+     */
+    handleInteraction(clickedButton) {
+        if (clickedButton.tagName === "BUTTON") {
             clickedButton.disabled = true;
-            if(!this.activePhrase.checkLetter(clickedButton.textContent)){
+            if (!this.activePhrase.checkLetter(clickedButton.textContent)) {
                 clickedButton.classList.add('wrong');
                 this.removeLife();
             } else {
                 clickedButton.classList.add('chosen');
                 this.activePhrase.showMatchedLetter(clickedButton.textContent);
-                if(this.checkForWin()){
+                if (this.checkForWin()) {
                     this.gameOver();
                 }
             }
@@ -54,60 +54,77 @@ class Game{
     /**
      * select all buttons and if the textcontent matches the typed letter run the click() method on it to simulate a mouseclick so the above methods get triggered.
      */
-    handleKeyboard(key){
+    handleKeyboard(key) {
         document.querySelectorAll('.key').forEach(button => {
-            if(key === button.textContent){
+            if (key === button.textContent) {
                 button.click();
             }
         });
     }
 
-     /**
-      * remove a liveHeart and increase the missed counter by one
-      * when player hits 5 misses run gameOver method.
-      */
-    removeLife(){
-        document.querySelectorAll('[src="images/liveHeart.png"]')[0].src = 'images/lostHeart.png';
-        this.missed ++;
-        if(this.missed === 5){
-            this.gameOver();
-        }
+    /**
+     * set pointer events to none during the animation to avoid animation from breaking
+     * start lightsaber animation to remove a liveHeart and increase the missed counter by one
+     * when player hits 5 misses run gameOver method.
+     */
+    removeLife() {
+        document.querySelectorAll('.key').forEach(key => key.style.pointerEvents = "none");
+        document.querySelector('.saber').classList.add('active');
+        // move lightsaber so it hits the correct heart
+        document.querySelector('.lightsaber').style.right = `${-35 * this.missed}px`;
+        setTimeout(() => {
+            document.querySelectorAll('[src="images/liveStormtrooper.png"]')[0].src = 'images/lostStormtrooper.png';
+        }, 1700);
+        setTimeout(() => {
+            document.querySelectorAll('.key').forEach(key => key.style.pointerEvents = "initial");
+            document.querySelector('.saber').classList.remove('active');
+            this.missed++;
+
+            if (this.missed === 5) {
+                this.gameOver();
+            }
+        }, 2500);
+
     }
 
-     /**
-      * Check if the game has won by comparing the amount of letters in the phrase and the amount of elements with the show class.
-      */
-    checkForWin(){
+    /**
+     * Check if the game has won by comparing the amount of letters in the phrase and the amount of elements with the show class.
+     */
+    checkForWin() {
         const letters = document.querySelectorAll('.letter');
         const chosen = document.querySelectorAll('.show');
         return letters.length === chosen.length;
     }
 
-     /**
-      * display the Overlay again and change the classname to either win or lose with a message about the result. 
-      */
-    gameOver(){
+    /**
+     * display the Overlay again and change the classname to either win or lose with a message about the result. 
+     */
+    gameOver() {
         const overlay = document.querySelector('#overlay');
         overlay.style.display = 'flex';
-        if(this.checkForWin()){
+        if (this.checkForWin()) {
             overlay.className = 'win';
-            document.querySelector('#game-over-message').textContent = `Congratulations you've won!`;
+            document.querySelector('#game-over-message').textContent = `At last the Jedi are no more`;
+            document.querySelector('#btn__reset').textContent = `Play again`;
         } else {
             overlay.className = 'lose';
-            document.querySelector('#game-over-message').textContent = `Better luck next time`;
+            document.querySelector('#game-over-message').textContent = `If so powerfull you are, Why leave?`;
+            document.querySelector('#btn__reset').textContent = `Play again`;
         }
     }
 
     /**
      * Reset the heart images, phrase ul and onscreen keyboard
      */
-    resetGame(){
-        document.querySelectorAll('[src="images/lostHeart.png"]').forEach( heart => heart.src = "images/liveHeart.png");
+    resetGame() {
+        document.querySelectorAll('[src="images/lostStormtrooper.png"]').forEach(heart => heart.src = "images/liveStormtrooper.png");
         this.missed = 0;
         document.querySelector('#phrase ul').innerHTML = '';
         document.querySelectorAll('.key').forEach(key => {
             key.className = 'key';
             key.disabled = false;
         });
+        // reset position of the lightsaber
+        document.querySelector('.lightsaber').style.right = `${-35 * this.missed}px`;
     }
 }
--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@
  * Project 4 - OOP Game App
  * app.js */
 
- let game;
+let game;
 
- document.querySelector('#btn__reset').addEventListener('click', () => {
-     game = new Game();
-     game.startGame();
- });
- 
- document.querySelector('#qwerty').addEventListener('click', e => {
-     game.handleInteraction(e.target);
- });
- document.addEventListener('keydown', e => game.handleKeyboard(e.key));
+document.querySelector('#btn__reset').addEventListener('click', () => {
+    game = new Game();
+    game.startGame();
+});
+
+document.querySelector('#qwerty').addEventListener('click', e => {
+    game.handleInteraction(e.target);
+});
+document.addEventListener('keydown', e => game.handleKeyboard(e.key));
